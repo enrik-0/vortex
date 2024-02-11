4e362432e962f391de63930b58054a7960f6e41b
Enrique Javier Villar Cea <EnriqueJavier.Villar@alu.uclm.es>
2024-02-11 12:35:37 +0100

    test?

--- test/src/test/java/vortex/test/StructureTest.java
+++ test/src/test/java/vortex/test/StructureTest.java
@@ -1,8 +1,9 @@
 package vortex.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
