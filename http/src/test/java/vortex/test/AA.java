824f516a0ee5f232aed959dccd871a5f8389a048
Enrique Javier Villar Cea <EnriqueJavier.Villar@alu.uclm.es>
2024-02-14 10:36:29 +0100

    a

---
+++ http/src/test/java/vortex/test/AA.java
@@ -0,0 +1,14 @@
+package vortex.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AA {
+
+
+	@Test
+	public void a() {
+		assertTrue(true);
+	}
+}
