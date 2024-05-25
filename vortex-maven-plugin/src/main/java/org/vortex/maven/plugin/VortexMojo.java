1c16f65ee6fa54d28d61e4b2cc0273620989129f
Enrique Javier Villar Cea <EnriqueJavier.Villar@alu.uclm.es>
2024-05-25 09:50:10 +0200

    added the plugin with a rare compile error

---
+++ vortex-maven-plugin/src/main/java/org/vortex/maven/plugin/VortexMojo.java
@@ -0,0 +1,15 @@
+package org.vortex.maven.plugin;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "run")
+public class VortexMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+	getLog().info("buenas");
+
+    }
+}
