9d620bec7f81fa61b8e814df71df421119d9ea0b
Enrique Javier Villar Cea <EnriqueJavier.Villar@alu.uclm.es>
2024-08-15 13:24:47 +0200

    docs(test): solve errors in javadoc
    errors in  javadoc solved

--- test/src/main/java/vortex/test/RequestBuilder.java
+++ test/src/main/java/vortex/test/RequestBuilder.java
@@ -1,4 +1,5 @@
 package vortex.test;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import vortex.annotate.constants.HttpMethod;
@@ -24,266 +26,285 @@ import vortex.test.exception.AmbiguousMethodException;
 import vortex.utils.HttpUtils;
 import vortex.utils.MappingUtils;
 
-public class RequestBuilder {
+public
+class RequestBuilder{
 
 
-	private static final int AMMOUNT_TO_READ = 255;
-	private static final int SECOND_10 = 10000;
-	private String uri;
-	private HttpMethod method;
-	private int timeout = SECOND_10;
-	private Map<String, String> headers;
-	private Object body;
+    private static final int AMMOUNT_TO_READ = 255;
+    private static final int SECOND_10 = 10000;
+    private String uri;
+    private HttpMethod method;
+    private int timeout = SECOND_10;
+    private Map<String, String> headers;
+    private Object body;
 
-	public RequestBuilder() {
-		headers = new HashMap<>();
-	}
+    public
+    RequestBuilder(){
+        headers = new HashMap<>();
+    }
 
-	public RequestBuilder(Map<String, String> headers) {
-		this.headers = new HashMap<>();
-		headers.forEach(this::addHeader);
-	}
+    public
+    RequestBuilder(Map<String, String> headers){
+        this.headers = new HashMap<>();
+        headers.forEach(this::addHeader);
+    }
 
-	public String getRequestUri() {
-		return uri;
-	}
+    /**
+     * @param name
+     * @param value
+     * @return {@link #RequestBuilder}
+     */
+    public
+    RequestBuilder addHeader(String name, String value){
+        headers.put(name, value);
+        return this;
 
-	public HttpMethod getRequestMethod() {
-		return method;
-	}
+    }
 
-	public int getTimeout() {
-		return timeout;
-	}
+    private static
+    Response createResponse(HttpURLConnection connection, InputStream input) throws IOException{
 
-	public RequestBuilder setBody(Object set) {
+        Response response = new ResponseStatus<Object>(getResponseBody(connection, input));
+        response.setStatus(HttpStatus.resolve(connection.getResponseCode()));
+        connection.getHeaderFields().forEach(response::setHeader);
 
-		if(method != HttpMethod.GET){
-		body = set;
-		}
-		return this;
-	}
-	/**
-	 * As this returns a copy of the headers modifications on it don't have any
-	 * result in the builder.
-	 * 
-	 * @return {@link #clone()}
-	 */
-	public Map<String, String> getRequestHeaders() {
-		Map<String, String> copy = new HashMap<>();
-		headers.forEach(copy::put);
-		return copy;
+        return response;
 
-	}
-	private void setUp(String uri, HttpMethod method)
-			throws AmbiguousMethodException {
-		if (this.method == null) {
-			this.uri = uri;
-			this.method = method;
-		} else {
-			throw new AmbiguousMethodException(String.format(
-					"%s %s %s %s %s %s",
-					"Creating a request using 2 methods at the same time is not allowed",
-					"\n", "request already have ", this.method.name(),
-					"and trying to put", method.name()));
-		}
+    }
 
-	}
-	/**
-	 * Sets request method to {@link HttpMethod.GET}
-	 * 
-	 * @param uri
-	 */
-	public RequestBuilder get(String uri) throws AmbiguousMethodException {
-		setUp(uri, HttpMethod.GET);
-		return this;
-	}
-	/**
-	 * Sets request method to {@link HttpMethod.POST}
-	 * 
-	 * @param uri
-	 */
-	public RequestBuilder post(String uri) throws AmbiguousMethodException {
-		setUp(uri, HttpMethod.POST);
-		return this;
-	}
-	/**
-	 * Sets request method to {@link HttpMethod.PUT}
-	 * 
-	 * @param uri
-	 */
-	public RequestBuilder put(String uri) throws AmbiguousMethodException {
-		setUp(uri, HttpMethod.PUT);
-		return this;
-	}
-	/**
-	 * Sets request method to {@link HttpMethod.DELETE}
-	 * 
-	 * @param uri
-	 */
-	public RequestBuilder delete(String uri) throws AmbiguousMethodException {
-		setUp(uri, HttpMethod.DELETE);
-		return this;
-	}
+    private static
+    Object getResponseBody(HttpURLConnection connection, InputStream inputStream) throws IOException{
+        try{
+            Object mapped = null;
+            if(inputStream != null){
+                var byteArrayOutputStream = copyInputStream(inputStream);
+                String contentHeader = connection.getHeaderField("Content-type");
+                if("application/json".equals(contentHeader)){
 
-	/**
-	 * Sets a specified timeout value, in milliseconds, to be used when opening
-	 * a communications link to the resource referenced by this uri. If the
-	 * timeout expires before the connection can be established, a
-	 * {@link java.net.SocketTimeoutException} is raised. A timeout of zero is
-	 * interpreted as an infinite timeout.
-	 * 
-	 * @param timeout
-	 */
-	public RequestBuilder setTimeout(int timeout) {
-		this.timeout = timeout;
-		return this;
-	}
+                    try{
+                        mapped = MappingUtils.map(byteArrayOutputStream.toByteArray(), Map.class);
+                    }catch(Exception e){
+                        mapped = MappingUtils.map(byteArrayOutputStream.toByteArray(), ArrayList.class);
+                    }
+                }
+                else{
+                    mapped = getResponseBody(byteArrayOutputStream.toByteArray());
 
-	/**
-	 * 
-	 * @param name
-	 * @param value
-	 * @return {@link #RequestBuilder}
-	 */
-	public RequestBuilder addHeader(String name, String value) {
-		headers.put(name, value);
-		return this;
+                }
+            }
 
-	}
-	private RequestBuilder setHeaders(HttpURLConnection connection) {
-		headers.forEach(connection::setRequestProperty);
-		return this;
-
-	}
-	/**
-	 * Sends the request and return the response
-	 * 
-	 * @return {@link Response}
-	 * @throws MalformedURLException
-	 * @throws SocketException
-	 * @throws ProtocolException
-	 * @throws IOException
-	 */
-	public Response perform() throws IOException {
-		URL url = new URL(uri);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setDoOutput(true);
-		try {
-			connection.setRequestMethod(this.method.name());
-			List<String> contentHeaders = new ArrayList<>();
-			HttpUtils.setContentHeader(contentHeaders, body,
-					headers.get("Content-type")).forEach(value -> {
-						this.addHeader("Content-type", value);
-					});
-			setHeaders(connection);
-			createBody(connection);
-			connection.setConnectTimeout(this.timeout);
-			InputStream stream = connection.getInputStream();
-			Response e = createResponse(connection, stream);
-			return e;
-		} catch (FileNotFoundException e) {
-			return createResponse(connection, connection.getErrorStream());
-		} catch (IOException e) {
-			Object  content = getResponseBody(connection, connection.getErrorStream());
-			return new ResponseStatus<>(HttpStatus.resolve(connection.getResponseCode()), content);
-		}
-	}
-
-	private static Response createResponse(HttpURLConnection connection,
-			InputStream input) throws IOException {
-
-		Response response = new ResponseStatus<Object>(
-				getResponseBody(connection, input));
-		response.setStatus(HttpStatus.resolve(connection.getResponseCode()));
-		connection.getHeaderFields().forEach(response::setHeader);
-
-		return response;
-
-	}
-
-	private void createBody(HttpURLConnection connection)
-			throws JsonProcessingException {
-		if (body != null) {
-			var bytes = MappingUtils.writeValueAsBytes(body);
-			ObjectMapper mapper = new ObjectMapper();
-
-			bytes = mapper.writeValueAsString(body).getBytes();
-			bytes = mapper.writeValueAsBytes(body);
-
-			try (OutputStream os = connection.getOutputStream()) {
-				os.write(bytes, 0, bytes.length);
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private static Object getResponseBody(HttpURLConnection connection,
-			InputStream inputStream) throws IOException {
-		try {
-			Object mapped = null;
-        if(inputStream != null){
-			var byteArrayOutputStream = copyInputStream(inputStream);
-			String contentHeader = connection.getHeaderField("Content-type");
-			if ("application/json".equals(contentHeader)) {
-
-				try {
-					mapped = MappingUtils.map(
-							byteArrayOutputStream.toByteArray(), Map.class);
-				} catch (Exception e) {
-					mapped = MappingUtils.map(
-							byteArrayOutputStream.toByteArray(),
-							ArrayList.class);
-				}
-			} else {
-				mapped = getResponseBody(byteArrayOutputStream.toByteArray());
-
+            return mapped;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
         }
-			}
 
-			return mapped;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+    }
 
-	}
-	private static Object getResponseBody(byte[] content) throws IOException {
-		Long temp;
-		char[] chars;
-		Object body = null;
-		var builder = new StringBuilder();
-		String buffer = MappingUtils
-				.getInputContent(new ByteArrayInputStream(content));
+    private static
+    Object getResponseBody(byte[] content) throws IOException{
+        Long temp;
+        char[] chars;
+        Object body = null;
+        var builder = new StringBuilder();
+        String buffer = MappingUtils.getInputContent(new ByteArrayInputStream(content));
 
-		if (buffer.startsWith("\"") && buffer.endsWith("\"")) {
-			chars = buffer.toCharArray();
-			for (var i = 1; i < chars.length - 1; i++) {
-				builder.append(chars[i]);
-			}
-			buffer = builder.toString();
-		}
-		body = MappingUtils.mapToPrimitive(content, buffer);
+        if(buffer.startsWith("\"") && buffer.endsWith("\"")){
+            chars = buffer.toCharArray();
+            for(var i = 1; i < chars.length - 1; i++){
+                builder.append(chars[i]);
+            }
+            buffer = builder.toString();
+        }
+        body = MappingUtils.mapToPrimitive(content, buffer);
 
-		return body;
-	}
+        return body;
+    }
 
+    private static
+    ByteArrayOutputStream copyInputStream(InputStream inputStream) throws IOException{
+        int longitud;
+        var buffer = new byte[AMMOUNT_TO_READ];
+        var byteArrayOutputStream = new ByteArrayOutputStream();
 
+        var r = inputStream.available();
+        while((longitud = inputStream.read(buffer)) != -1){
+            byteArrayOutputStream.write(buffer, 0, longitud);
+        }
 
-	private static ByteArrayOutputStream copyInputStream(
-			InputStream inputStream) throws IOException {
-		int longitud;
-		var buffer = new byte[AMMOUNT_TO_READ];
-		var byteArrayOutputStream = new ByteArrayOutputStream();
+        return byteArrayOutputStream;
+    }
 
-		var r = inputStream.available();
-		while ( (longitud = inputStream.read(buffer)) != -1) {
-			byteArrayOutputStream.write(buffer, 0, longitud);
-		}
+    public
+    String getRequestUri(){
+        return uri;
+    }
 
-		return byteArrayOutputStream;
-	}
+    public
+    HttpMethod getRequestMethod(){
+        return method;
+    }
+
+    public
+    int getTimeout(){
+        return timeout;
+    }
+
+    /**
+     * Sets a specified timeout value, in milliseconds, to be used when opening
+     * a communications link to the resource referenced by this uri. If the
+     * timeout expires before the connection can be established, a
+     * {@link java.net.SocketTimeoutException} is raised. A timeout of zero is
+     * interpreted as an infinite timeout.
+     *
+     * @param timeout
+     */
+    public
+    RequestBuilder setTimeout(int timeout){
+        this.timeout = timeout;
+        return this;
+    }
+
+    public
+    RequestBuilder setBody(Object set){
+
+        if(method != HttpMethod.GET){
+            body = set;
+        }
+        return this;
+    }
+
+    /**
+     * As this returns a copy of the headers modifications on it don't have any
+     * result in the builder.
+     *
+     * @return {@link #clone()}
+     */
+    public
+    Map<String, String> getRequestHeaders(){
+        Map<String, String> copy = new HashMap<>();
+        headers.forEach(copy::put);
+        return copy;
+
+    }
+
+    /**
+     * Sets request method to GET
+     *
+     * @param uri
+     */
+    public
+    RequestBuilder get(String uri) throws AmbiguousMethodException{
+        setUp(uri, HttpMethod.GET);
+        return this;
+    }
+
+    private
+    void setUp(String uri, HttpMethod method) throws AmbiguousMethodException{
+        if(this.method == null){
+            this.uri = uri;
+            this.method = method;
+        }
+        else{
+            throw new AmbiguousMethodException(String.format("%s %s %s %s %s %s",
+                                                             "Creating a request using 2 methods at the same time is " +
+															 "not allowed",
+                                                             "\n", "request already have ", this.method.name(),
+                                                             "and trying to put", method.name()));
+        }
+
+    }
+
+    /**
+     * Sets request method to POST
+     *
+     * @param uri
+     */
+    public
+    RequestBuilder post(String uri) throws AmbiguousMethodException{
+        setUp(uri, HttpMethod.POST);
+        return this;
+    }
+
+    /**
+     * Sets request method to PUT
+     *
+     * @param uri
+     */
+    public
+    RequestBuilder put(String uri) throws AmbiguousMethodException{
+        setUp(uri, HttpMethod.PUT);
+        return this;
+    }
+
+    /**
+     * Sets request method to DELETE
+     *
+     * @param uri
+     */
+    public
+    RequestBuilder delete(String uri) throws AmbiguousMethodException{
+        setUp(uri, HttpMethod.DELETE);
+        return this;
+    }
+
+    private
+    RequestBuilder setHeaders(HttpURLConnection connection){
+        headers.forEach(connection::setRequestProperty);
+        return this;
+
+    }
+
+    /**
+     * Sends the request and return the response
+     *
+     * @return {@link Response}
+     * @throws MalformedURLException
+     * @throws SocketException
+     * @throws ProtocolException
+     * @throws IOException
+     */
+    public
+    Response perform() throws IOException{
+        URL url = new URL(uri);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        try{
+            connection.setRequestMethod(this.method.name());
+            List<String> contentHeaders = new ArrayList<>();
+            HttpUtils.setContentHeader(contentHeaders, body, headers.get("Content-type")).forEach(value -> {
+                this.addHeader("Content-type", value);
+            });
+            setHeaders(connection);
+            createBody(connection);
+            connection.setConnectTimeout(this.timeout);
+            InputStream stream = connection.getInputStream();
+            Response e = createResponse(connection, stream);
+            return e;
+        }catch(FileNotFoundException e){
+            return createResponse(connection, connection.getErrorStream());
+        }catch(IOException e){
+            Object content = getResponseBody(connection, connection.getErrorStream());
+            return new ResponseStatus<>(HttpStatus.resolve(connection.getResponseCode()), content);
+        }
+    }
+
+    private
+    void createBody(HttpURLConnection connection) throws JsonProcessingException{
+        if(body != null){
+            var bytes = MappingUtils.writeValueAsBytes(body);
+            ObjectMapper mapper = new ObjectMapper();
+
+            bytes = mapper.writeValueAsString(body).getBytes();
+            bytes = mapper.writeValueAsBytes(body);
+
+            try(OutputStream os = connection.getOutputStream()){
+                os.write(bytes, 0, bytes.length);
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
